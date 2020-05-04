af2053991dd8f23960222110d8a327e73d15a6c9
vagrawal48 <vadini@gatech.edu>
2020-05-04 12:08:41 -0500

    Added cors

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,9 @@ const functions = require('firebase-functions');
 const app = require('express')();
 const FBAuth = require('./util/fbAuth');
 
+const cors = require('cors');
+app.use(cors());
+
 const { db } = require('./util/admin');
 
 const { getAllUpdates, postOneUpdate, getUpdate, commentOnUpdate, deleteUpdate, likeUpdate, unlikeUpdate} = require('./handlers/updates');
